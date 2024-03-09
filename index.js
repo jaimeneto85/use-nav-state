4f6d48c8e00af53c6d2fe2d0fbd6399d9f2c5aab
Jaime Lima Neto <jaimeneto85@gmail.com>
2024-03-09 12:58:21 -0300

    chenge to js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-type TUseNavState = [value: string, setValue: (value: string) => void];
-
-export function useNavState(key: string, initialValue?: string) : TUseNavState {
+export function useNavState(key, initialValue) {
   const [value, setValue] = useState(() => {
     const params = new URLSearchParams(window.location.search);
     return params.get(key) || initialValue;
@@ -14,7 +12,7 @@ export function useNavState(key: string, initialValue?: string) : TUseNavState {
     else params.delete(key);
     console.log(params.toString());
     const newUrl = `${window.location.pathname}?${params.toString()}`;
-    window.history.pushState({}, '', newUrl);
+    window.history.pushState({}, "", newUrl);
   }, [value, key]);
 
   return [value, setValue];
